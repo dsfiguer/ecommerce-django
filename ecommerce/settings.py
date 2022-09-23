038c7db8ed7e7ac46551be6499a47397d2adb561
David Figueroa <dsfigueroa.software@gmail.com>
2022-09-22 21:56:55 -0700

    Switched env setup for DO apps integration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,16 +1,9 @@
-import environ
-import os
-
+from os import environ as env
 from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Setting up environment variables
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR.parent, 'environment', '.env'))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
