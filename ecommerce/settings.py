f736a6acd584babf338b41da811c9007416bc042
David Figueroa <dsfigueroa.software@gmail.com>
2022-09-27 15:11:17 -0700

    Modified dev STATIC_URL

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,6 +138,8 @@ if not DEBUG:
     AWS_S3_FILE_OVERWRITE = True
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
